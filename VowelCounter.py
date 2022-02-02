81a00403bac8cc4902a884bdc18956763526b8b1
frantizek <fruvalc@gmail.com>
2022-02-02 13:57:43 -0600

    simple gui version

--- VowelCounter.py
+++ VowelCounter.py
@@ -1,20 +1,54 @@
-CountVowels = str(input("Escribe una frase o texto: "))
+from tkinter import *
 
-d_vowels = {"a": 0,  "e": 0, "i": 0, "o": 0, "u": 0}
+root = Tk()
+root.geometry("600x500")
+root.title("Cuenta Vocales")
 
-for letter in CountVowels:
-    if "a" in letter.lower():
-        d_vowels["a"] = d_vowels["a"] + 1
-    if "e" in letter.lower():
-        d_vowels["e"] = d_vowels["e"] + 1
-    if "i" in letter.lower():
-        d_vowels["i"] = d_vowels["i"] + 1
-    if "o" in letter.lower():
-        d_vowels["o"] = d_vowels["o"] + 1
-    if "u" in letter.lower():
-        d_vowels["u"] = d_vowels["u"] + 1
 
-print("\nEl texto a analizar tiene {} caracteres:".format(len(CountVowels)))
+def VowelCounter():
+    Output.delete('1.0', END)
+    CountVowels = str(inputtxt.get("1.0", "end-1c"))
+    d_vowels = {"a": 0,  "e": 0, "i": 0, "o": 0, "u": 0}
 
-for key, value in d_vowels.items():
-    print("La vocal '{}' aparece {} veces.".format(key, value))
+    for letter in CountVowels:
+        if "a" in letter.lower():
+            d_vowels["a"] = d_vowels["a"] + 1
+        if "e" in letter.lower():
+            d_vowels["e"] = d_vowels["e"] + 1
+        if "i" in letter.lower():
+            d_vowels["i"] = d_vowels["i"] + 1
+        if "o" in letter.lower():
+            d_vowels["o"] = d_vowels["o"] + 1
+        if "u" in letter.lower():
+            d_vowels["u"] = d_vowels["u"] + 1
+
+    TerribleLongAnswer = ""
+
+    TerribleLongAnswer = TerribleLongAnswer + "El texto a analizar tiene {} caracteres:\n".format(len(CountVowels))
+
+    for key, value in d_vowels.items():
+        TerribleLongAnswer = TerribleLongAnswer + "\nLa vocal '{}' aparece {} veces.".format(key, value)
+
+    Output.insert(END, TerribleLongAnswer)
+
+
+l = Label(text="\nEscribe una frase o texto: \n")
+inputtxt = Text(root, height=10,
+                width=60,
+                bg="light yellow")
+
+Output = Text(root, height=10,
+              width=50,
+              bg="white")
+
+Display = Button(root, height=2,
+                 width=20,
+                 text="Cuenta las vocales",
+                 command=lambda: VowelCounter())
+
+l.pack()
+inputtxt.pack()
+Display.pack()
+Output.pack()
+
+mainloop()
