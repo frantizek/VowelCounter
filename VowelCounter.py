fd3e391dff8882172796e866d4a5c60011a17c4e
José Francisco Ruvalcaba Castañeda <32653773+frantizek@users.noreply.github.com>
2024-03-20 13:47:43 -0600

    Update VowelCounter.py
    use customtkinter as an app

--- VowelCounter.py
+++ VowelCounter.py
@@ -1,56 +1,62 @@
 import customtkinter
 
 customtkinter.set_appearance_mode("Light")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
+
+class MyVocalsFrame(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.l2 = customtkinter.CTkLabel(self, text="\nEscribe una frase o texto: \n",
+                                    font=customtkinter.CTkFont(size=16, weight="normal"))
+        self.l2.grid(row=0, column=0, padx=10, pady=5)
+
+        self.inputtxt = customtkinter.CTkTextbox(self, width=400, corner_radius=0, bg_color="white")
+        self.inputtxt.grid(row=1, column=0, padx=10, pady=5)
+
+        def VowelCounter():
+            self.outputtxt.delete('0.0', "end")
+            CountVowels = str(self.inputtxt.get("0.0", "end"))
+            d_vowels = {"a": 0,  "e": 0, "i": 0, "o": 0, "u": 0}
+
+            for letter in CountVowels:
+                if "a" in letter.lower():
+                    d_vowels["a"] = d_vowels["a"] + 1
+                if "e" in letter.lower():
+                    d_vowels["e"] = d_vowels["e"] + 1
+                if "i" in letter.lower():
+                    d_vowels["i"] = d_vowels["i"] + 1
+                if "o" in letter.lower():
+                    d_vowels["o"] = d_vowels["o"] + 1
+                if "u" in letter.lower():
+                    d_vowels["u"] = d_vowels["u"] + 1
+
+            TerribleLongAnswer = ""
+
+            TerribleLongAnswer = TerribleLongAnswer + "El texto a analizar tiene {} caracteres:\n".format(len(CountVowels))
+
+            for key, value in d_vowels.items():
+                TerribleLongAnswer = TerribleLongAnswer + "\nLa vocal '{}' aparece {} veces.".format(key, value)
+
+            self.outputtxt.insert("0.0", TerribleLongAnswer)
+
+        self.display = customtkinter.CTkButton(self, text="Cuenta las vocales.", width=28, command=lambda: VowelCounter())
+        self.display.grid(row=2, column=0, padx=10, pady=5)
+
+        self.outputtxt = customtkinter.CTkTextbox(self, width=400, corner_radius=0, bg_color="gray")
+        self.outputtxt.grid(row=3, column=0, padx=10, pady=5)
 
 
-root = customtkinter.CTk()
-root.geometry("600x700")
-root.title("Cuenta Vocales")
-
-frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20, padx=60, fill="both", expand=True)
-
-
-l2 = customtkinter.CTkLabel(master=frame, text="\nEscribe una frase o texto: \n",
-                            font=customtkinter.CTkFont(size=16, weight="normal"))
-l2.pack(padx=20, pady=10)
-
-inputtxt = customtkinter.CTkTextbox(master=frame, width=400, corner_radius=0, bg_color="white")
-inputtxt.pack(padx=20, pady=10)
-
-outputtxt = customtkinter.CTkTextbox(master=frame, width=400, corner_radius=0, bg_color="gray")
-outputtxt.pack(padx=20, pady=10)
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry(f"{600}x{700}")
+        self.title("Cuenta Vocales")
+        self.grid_rowconfigure(2, weight=1)  # configure grid system
+        self.grid_columnconfigure(1, weight=1)
+        self.my_frame = MyVocalsFrame(master=self)
+        self.my_frame.grid(row=2, column=1, padx=10, pady=10)
 
 
-def VowelCounter():
-    outputtxt.delete('0.0', "end")
-    CountVowels = str(inputtxt.get("0.0", "end"))
-    d_vowels = {"a": 0,  "e": 0, "i": 0, "o": 0, "u": 0}
-
-    for letter in CountVowels:
-        if "a" in letter.lower():
-            d_vowels["a"] = d_vowels["a"] + 1
-        if "e" in letter.lower():
-            d_vowels["e"] = d_vowels["e"] + 1
-        if "i" in letter.lower():
-            d_vowels["i"] = d_vowels["i"] + 1
-        if "o" in letter.lower():
-            d_vowels["o"] = d_vowels["o"] + 1
-        if "u" in letter.lower():
-            d_vowels["u"] = d_vowels["u"] + 1
-
-    TerribleLongAnswer = ""
-
-    TerribleLongAnswer = TerribleLongAnswer + "El texto a analizar tiene {} caracteres:\n".format(len(CountVowels))
-
-    for key, value in d_vowels.items():
-        TerribleLongAnswer = TerribleLongAnswer + "\nLa vocal '{}' aparece {} veces.".format(key, value)
-
-    outputtxt.insert("0.0", TerribleLongAnswer)
-
-
-display = customtkinter.CTkButton(master=frame, text="Cuenta las vocales.", height=2, width=20, command=lambda: VowelCounter())
-display.pack(padx=20, pady=10)
-
-root.mainloop()
+app = App()
+app.mainloop()
